980477cbf9c314f3014a1e76154bdc68a04995dc
hbrandt <h.brandt@steinberg.de>
2025-04-23 14:59:45 +0200

    add audio analyzer

--- src/lib/audio-engine.ts
+++ src/lib/audio-engine.ts
@@ -1,12 +1,30 @@
 import * as Tone from 'tone';
 
+export const oscAnalyser = new Tone.Analyser('waveform', 1024);
+export const outputAnalyser = new Tone.Analyser('waveform', 1024);
+
 export const filter = new Tone.Filter({
   type: 'lowpass',
   frequency: 800,
   Q: 1,
-}).toDestination();
+});
 
-export const synth = new Tone.Synth().connect(filter);
+export const synth = new Tone.Synth({
+  oscillator: {
+    type: 'sine',
+  },
+  envelope: {
+    attack: 0.01,
+    decay: 0.2,
+    sustain: 0.7,
+    release: 0.3,
+  },
+});
+
+synth.connect(oscAnalyser);
+oscAnalyser.connect(filter);
+filter.connect(outputAnalyser);
+outputAnalyser.toDestination();
 
 export const initAudioContext = async () => {
   await Tone.start();
